39fed6b07d4e6252322e0089db29bd4edc4e9ffa
Joris Jansen <49146451+Joris1Jansen@users.noreply.github.com>
2021-10-05 06:22:17 +0200

    Revert "implement JWT"

--- src/main/java/com/rentmycar/rentmycar/security/config/JwtConfig.java
+++
@@ -1,6 +0,0 @@
-package com.rentmycar.rentmycar.security.config;
-
-public class JwtConfig {
-    public static final String SECRET_KEY = "f9c48b57-24bb-4313-94c4-0b36aece7bd7";
-    public static final int EXPIRATION_TIME = 86400000;
-}
