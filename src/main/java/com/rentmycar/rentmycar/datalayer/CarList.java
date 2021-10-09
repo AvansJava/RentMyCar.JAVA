dbd44ace6e4413c038dfe3a1a623907bb65b94aa
Joris Jansen <j.jansen@ispnext.com>
2021-10-09 20:59:28 +0200

    WIP

--- src/main/java/com/rentmycar/rentmycar/datalayer/CarList.java
+++ src/main/java/com/rentmycar/rentmycar/datalayer/CarList.java
@@ -1,10 +1,19 @@
 package com.rentmycar.rentmycar.datalayer;
 
-public class CarList {
-    private String Brand;
+import java.util.HashMap;
+import java.util.Map;
 
-    public String getBrand() {
-        return Brand;
+public class CarList {
+
+    private String Brand;
+    private String Model;
+
+    public Map<String, String> getBrand() {
+        Map<String, String> carList = new HashMap<>();
+        carList.put("brand", Brand);
+        carList.put("model", Model);
+
+        return carList;
     }
 
     public void setBrand(String brand) {
