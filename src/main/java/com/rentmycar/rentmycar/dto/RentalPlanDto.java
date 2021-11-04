29fe240901ac61cf1e2f5255aaf0195df22936d5
zafuso <71095771+zafuso@users.noreply.github.com>
2021-11-04 16:15:44 +0100

    fix rentalplan dto date

--- src/main/java/com/rentmycar/rentmycar/dto/RentalPlanDto.java
+++ src/main/java/com/rentmycar/rentmycar/dto/RentalPlanDto.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,8 +13,8 @@ public class RentalPlanDto {
     private Long id;
     private Long carId;
     private Long userId;
-    private LocalDateTime availableFrom;
-    private LocalDateTime availableUntil;
+    private LocalDate availableFrom;
+    private LocalDate availableUntil;
     private BigDecimal price;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
